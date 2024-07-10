d987ddc04a8e7cd3a62df70bf9c259bba5da2cc9
Z-noob <Zhenniwu5@gmail.com>
2024-07-10 15:40:02 -0400

    all

--- vite-project/src/model/ShoppingCart.js
+++ vite-project/src/model/ShoppingCart.js
@@ -2,7 +2,41 @@ import getId from "../utils/getId";
 import CartItem from "./CartItem";
 
 class ShoppingCart {
+  static #allCarts = [];
+  #cartItems = [];
 
+  constructor() {
+    this.id = getId();
+    // Array to store books
+    ShoppingCart.#allCarts.push(this); 
+  };
+
+  createItem(name, price) {
+    const item = new CartItem(name, price);
+    this.#cartItems.push(item);
+    return item;
+  };
+
+  getItems() {
+    return [...this.#cartItems];
+  };
+
+  getTotal() {
+    console.log(this.#cartItems)
+    return this.#cartItems.reduce((acc, {price}) => acc + price, 0);
+  };
+
+  removeItem(id) {
+    const indexToDelete = this.#cartItems.findIndex(obj => obj.id === id);
+    if (id !== -1) {
+      this.#cartItems.splice(indexToDelete, 1);
+    };
+  };
+  static listAll() {
+    return [...ShoppingCart.#allCarts]
+  }
+  static findBy(id) {
+    return (ShoppingCart.#allCarts.filter(obj => obj.id === id))[0];
+  }
 }
-
 export default ShoppingCart;
